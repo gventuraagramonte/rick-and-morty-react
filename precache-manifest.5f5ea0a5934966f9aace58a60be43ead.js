32d31dceecac830f195f8e803e8061f20bc44240
Giorgio Ventura <gventuraagramonte@gmail.com>
2020-08-24 00:04:40 -0500

    Updates

---
+++ precache-manifest.5f5ea0a5934966f9aace58a60be43ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678d9b6d3fbc3764bad44bc7190d65d9",
+    "url": "/rick-and-morty-react/index.html"
+  },
+  {
+    "revision": "66302f317e7c2eff9a97",
+    "url": "/rick-and-morty-react/static/css/main.1fbc11e1.chunk.css"
+  },
+  {
+    "revision": "a809ee17b5d6df60a5fd",
+    "url": "/rick-and-morty-react/static/js/2.b39d4ff8.chunk.js"
+  },
+  {
+    "revision": "a9dd647aec81fffade4fa7e6d693dad7",
+    "url": "/rick-and-morty-react/static/js/2.b39d4ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66302f317e7c2eff9a97",
+    "url": "/rick-and-morty-react/static/js/main.9b1fb2a7.chunk.js"
+  },
+  {
+    "revision": "4782415dcb347d8db047",
+    "url": "/rick-and-morty-react/static/js/runtime-main.84ea4004.js"
+  }
+]);
